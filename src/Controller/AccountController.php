72783eba51074d80f053037b99b7e2986026ff31
kevin <kevinkalenga10@gmail.com>
2026-05-13 13:38:40 +0200

    Ajout, Modification et Suppression d'une adresse depuis l'espace membre de l'utilisateur

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -9,16 +9,33 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use App\Form\PasswordUserType;
+use App\Entity\Address;
+use App\Repository\AddressRepository;
+use App\Form\AddressUserType;
 
 final class AccountController extends AbstractController
 {
+    
+    private $entityManager; 
+
+
+    
+    
+    
+    
+    public function __construct( EntityManagerInterface $entityManager)
+    {
+      $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/compte', name: 'app_account')]
     public function index(): Response
     {
         return $this->render('account/index.html.twig');
     }
     #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modify_pwd')]
-    public function password(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    public function password(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         // Recup de user connecté
         $user = $this->getUser();
@@ -32,7 +49,7 @@ final class AccountController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            $this->entityManager->flush();
               $this->addFlash(
                type:'success',
                message:'Votre mot de passe a été mis à jour avec succès'
@@ -42,5 +59,75 @@ final class AccountController extends AbstractController
         return $this->render('account/password.html.twig', [
             'modifyPwd' =>$form->createView()
         ]);
+    }
+    
+    // Affichage du template pour les adresses
+    #[Route('/compte/adresses', name: 'app_account_addresses')]
+    public function addresses(): Response
+    {
+        return $this->render('account/addresses.html.twig');
+    }
+    
+    // Ajout d'adresse
+    #[Route('/compte/adresses/ajout/{id}', name: 'app_account_address_form', defaults: ['id' => null])]
+    public function addressForm(Request $request, $id, AddressRepository $addressRepository): Response
+    {
+        if($id) {
+            // L'adresse en bd
+           $address = $addressRepository->findOneById($id);
+           if(!$address or $address->getUser() != $this->getUser()) {
+               return $this->redirectToRoute('app_account_addresses');
+           }
+        } else {
+            $address = new Address();
+            $address->setUser($this->getUser());
+        }
+        
+        $form = $this->createForm(AddressUserType::class, $address);
+        $form->handleRequest($request);
+
+          if($form->isSubmitted() && $form->isValid()) {
+             $this->entityManager->persist($address);
+             $this->entityManager->flush();
+
+             $this->addFlash(
+               type:'success',
+               message:'Votre adresse a été ajoutée avec succès'
+            );
+
+            return $this->redirectToRoute('app_account_addresses');
+          }
+        
+        
+        
+        
+        
+        
+        return $this->render('account/addressForm.html.twig', [
+            'addressForm' => $form
+        ]);
+    }
+    // Suppression d'adresse
+    #[Route('/compte/adresses/suppression/{id}', name: 'app_account_address_delete')]
+    public function deleteForm(Request $request, $id, AddressRepository $addressRepository): Response
+    {
+  
+            // L'adresse en bd
+           $address = $addressRepository->findOneById($id);
+           
+           if(!$address or $address->getUser() != $this->getUser()) {
+               return $this->redirectToRoute('app_account_addresses');
+           }
+
+            $this->addFlash(
+               type:'success',
+               message:'Votre adress a été supprimée avec succès'
+            );
+      
+            $this->entityManager->remove($address);
+            $this->entityManager->flush();
+            return $this->redirectToRoute('app_account_addresses');    
+        
+       
     }
 }
---
+++ src/Form/AddressUserType.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class AddressUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre nom'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Votre adresse',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre adresse'
+                ]
+            ])
+            ->add('postal', TextType::class, [
+                'label' => 'Votre code postal',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre code postal'
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Votre ville',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre ville'
+                ]
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'Votre pays',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre pays'
+                ]
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Votre télephone',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre numero de télephone'
+                ]
+            ])
+            ->add('Submit', SubmitType::class, [
+                'label' => "Valider",
+                'attr' => [
+                     'class' => "btn btn-success"
+                ]
+            ])
+           
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
